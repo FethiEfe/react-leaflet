143d58fb3ff0b54f7408c163cda2166452818bff
FETHI AKCAY <f.akcay1@gmail.com>
2020-04-15 21:46:54 -0500

    feat: setting up leaflet and centering map on US location

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import L from "leaflet"
+import { Map, TileLayer, Marker, Popup } from "react-leaflet"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    lat: 35.505,
+    lng: -90.09,
+    zoom: 4,
+  }
+  render() {
+    const position = [this.state.lat, this.state.lng]
+    return (
+      <div className="App">
+        <Map className = "map" center={position} zoom={this.state.zoom}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+          </Popup>
+          </Marker>
+        </Map>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
